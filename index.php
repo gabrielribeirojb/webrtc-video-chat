58f6a2ff0b39dc846a41e24ce48e660dc944af65
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-02-15 13:52:04 -0300

    Criando marcação inicial

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src='https://cdn.scaledrone.com/scaledrone.min.js'></script>   
+    <title>Sistema video-chat</title>
+    <link src="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    
+    <div class="welcome">
+        <h2>Bem vido ao nosso chat em tempo real</h2>
+    </div><!--welcome-->
+
+    <video id="localVideo"></video>
+    <video id="remoteVideo"></video>
+
+    <script>
+        //Inicio scaledrone e WebRTC
+
+    </script>
+ 
+</body>
+</html>
